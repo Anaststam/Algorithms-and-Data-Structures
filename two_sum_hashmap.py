fa6b6d30aaba52084f366f3341863aa8b08e2307
MadameKisling <ge25vej@mytum.de>
2019-10-01 23:39:57 +0200

    Solution using Hashmaps. Time complexity O(n)

--- two_sum_hashmap.py
+++ two_sum_hashmap.py
@@ -1,20 +1,20 @@
-nums = [3,3]
-target = 6
-indices = []
-dict1 = {}
-for i in range(len(nums)):
-    compl = target - nums[i]
-    if dict1.get(nums[i]) is not None and compl == nums[i]:
-        indices.append(dict1[nums[i]])
-        indices.append(i)
-        break
-    else:
-        dict1.update({nums[i]: i})
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices = []
+        dict1 = {}
+        for i in range(len(nums)):
+            compl = target - nums[i]
+            if dict1.get(nums[i]) is not None and compl == nums[i]:
+                indices.append(dict1[nums[i]])
+                indices.append(i)
+                break
+            else:
+                dict1.update({nums[i]: i})
 
-        compl = target - nums[i]
-        if dict1.get(compl) is not None and compl != nums[i]:
-            indices.append(dict1[compl])
-            indices.append(i)
-            break
+                compl = target - nums[i]
+                if dict1.get(compl) is not None and compl != nums[i]:
+                    indices.append(dict1[compl])
+                    indices.append(i)
+                    break
 
-print(indices)
+        return indices
